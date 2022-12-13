14254268134d7ab7ac4d6eba04547b1b4cfaa6e9
Po-Sheng Chen <a7061120@gmail.com>
2022-12-13 22:25:21 +0800

    Fix match url

--- Duolingo Hide Completed Skills.user.js
+++ Duolingo Hide Completed Skills.user.js
@@ -4,7 +4,7 @@
 // @version      1.3
 // @description  A Duolingo userscripts that toggle hide/show completed skills
 // @author       Ponsheng
-// @match        https://duolingo.com/learn
+// @match        https://www.duolingo.com/learn
 // @match        https://preview.duolingo.com/learn
 // @grant        none
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
